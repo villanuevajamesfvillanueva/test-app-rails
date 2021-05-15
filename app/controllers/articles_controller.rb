ecac31c61da04d848c84b6f21cb194957162fb04
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-15 10:56:28 +0800

    added error messages for user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,16 +10,20 @@ class ArticlesController < ApplicationController
 
 
     def create
-        @article = Article.new
-        @article.name = params[:name]
-        @article.body = params[:body]
+        # @article = Article.new
+        # @article.name = params[:name]
+        # @article.body = params[:body]
+        @article = Article.new(article_params)
 
         if @article.valid?
             @article.save
             redirect_to articles_path
             
         else render :new
-            
-
     end
+end
+
+private
+def article_params
+    params.require(:article).permit(:name, :body)
 end
