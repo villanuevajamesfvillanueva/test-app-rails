ee17d64169f755cdc07d16edb40563dcfd4a45b8
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-18 00:42:33 +0800

    added new routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def create
         # @article = Article.new
         # @article.name = params[:name]
@@ -20,7 +24,6 @@ class ArticlesController < ApplicationController
             
         else
             render :new
-
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #get syntax: '<route>' => '<controller>#<method>'
-  # get '/articles' => 'articles#index'
   root 'articles#index'
+
+  get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show', as: 'articles_show'
   get '/articles/new' => 'articles#new'
   post '/articles' => 'articles#create', as: 'article_create'
   
