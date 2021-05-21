685cc3d9bdf2f32cf30632d5540a4931b146d25a
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-21 19:32:37 +0800

    updated notes and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,13 @@ Rails.application.routes.draw do
 
   get '/articles/new', to: 'articles#new', as: 'articles_new'
 
-  get '/articles/:id/edit', to: 'articles#edit', as: 'articles_edit'
+  get '/articles/:id/edit', to: 'articles#edit', as: 'articles_edit', id: /\d+/
   put '/articles/:id', to: 'articles#update', as: 'articles_update'
 
   delete '/articles/:id/delete', to: 'articles#destroy', as: 'articles_delete'
   # delete '/articles/:id', to: 'articles#destroy' as: 'articles_delete'
 
-  get '/articles/:id', to: 'articles#show', as: 'articles_show'
+  get '/articles/:id', to: 'articles#show', as: 'articles_show', id: /\d+/
 
   post '/articles' => 'articles#create', as: 'article_create'
   
